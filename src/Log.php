ce1be2459f08d1cd270c0f4dfacfc67c756cf818
Faraz Fallahi <fffaraz@gmail.com>
2020-07-18 20:50:25 -0400

    Update Log.php

--- src/Log.php
+++ src/Log.php
@@ -4,27 +4,39 @@ namespace fffaraz\Utils;
 
 class Log
 {
-    public static function message($message = '', $category = 'default', $ignoreAdmin = true)
+    public static function log($category = 'default', $message = '', $exception = null, $ignoreAdmin = true, $chatId = null)
     {
+        $userId = \Illuminate\Support\Facades\Auth::id();
+        if (!is_string($category)) $category = 'invalid';
+        if (!is_string($message)) $message = json_encode($message, JSON_UNESCAPED_UNICODE);
+        else if (strlen($message) < 1) $message = null;
+        if ($exception != null) {
+            echo 'Caught exception: ', $exception->getMessage(), "\n";
+            $exception = $exception->__toString();
+        }
         $log = [
-            'user_id' => \Illuminate\Support\Facades\Auth::id(),
+            'user_id' => $userId,
             'category' => $category,
-            'message' => is_string($message) ? $message : json_encode($message, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE),
             'ip' => \Request::ip(),
-            'agent' => \Request::header('User-Agent'),
+            'message' => $message,
+            'path' => \Request::path(),
+            'agent' => strval(\Request::header('User-Agent')),
+            'referer' => strval(\Request::server('HTTP_REFERER')),
+            'exception' => $exception,
         ];
         \Illuminate\Support\Facades\Redis::publish('log', print_r($log, true));
-        if ($ignoreAdmin && \Illuminate\Support\Facades\Auth::id() == 1) return;
+        if ($exception != null && $ignoreAdmin && $userId == 1) return $log;
         if (class_exists('\Debugbar')) \Debugbar::disable();
         \App\Models\Log::Create($log);
-    }
-
-    public static function exception(\Exception $e, $category = 'default')
-    {
-        echo 'Caught exception: ', $e->getMessage(), "\n";
-        \App\Models\Log::Create([
-            'category' => $category . ':exception',
-            'message' => $e->__toString(),
-        ]);
+        if ($chatId != null) {
+            $telegram = new \Telegram\Bot\Api();
+            $response = $telegram->sendMessage([
+                'chat_id' => $chatId,
+                'text'    => json_encode($log, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES),
+                'disable_web_page_preview' => true,
+                // 'parse_mode' => 'html',
+            ]);
+        }
+        return $log;
     }
 }
