45fc387e3c0e01232cb7aa29d95c9e6e0d111468
Faraz Fallahi <fffaraz@gmail.com>
2020-11-26 20:55:09 -0500

    Update Log.php

--- src/Log.php
+++ src/Log.php
@@ -7,31 +7,44 @@ class Log
     public static function log($category = 'default', $message = '', $exception = null, $ignoreAdmin = true, $chatId = null)
     {
         $userId = \Illuminate\Support\Facades\Auth::id();
+
         if (!is_string($category)) $category = json_encode($category, JSON_UNESCAPED_UNICODE);
-        if (!is_string($message)) $message = json_encode($message, JSON_UNESCAPED_UNICODE);
+
+        if (!is_string($message)) $messageStr = json_encode($message, JSON_UNESCAPED_UNICODE);
+        else $messageStr = $message;
+
         if ($exception != null) {
             echo 'Caught exception: ', $exception->getMessage(), "\n";
             $exception = $exception->__toString();
         }
+
         $ip = \Request::ip();
         $path = \Request::path();
         $agent = strval(\Request::header('User-Agent'));
         $referer = strval(\Request::server('HTTP_REFERER'));
+
         $log = [
             'user_id' => $userId,
             'category' => $category,
             'ip' => strlen($ip) > 0 ? $ip : null,
-            'message' => strlen($message) > 0 ? $message : null,
+            'message' => strlen($messageStr) > 0 ? $messageStr : null,
             'path' => strlen($path) > 0 ? $path : null,
             'agent' => strlen($agent) > 0 ? $agent : null,
             'referer' => strlen($referer) > 0 ? $referer : null,
             'exception' => $exception,
         ];
+
         \Illuminate\Support\Facades\Redis::publish('log', print_r($log, true));
+
         if ($exception == null && $ignoreAdmin && $userId == 1) return $log;
-        if (class_exists('\Debugbar')) \Debugbar::disable();
+
+        if ($userId != 1 && class_exists('\Debugbar')) \Debugbar::disable();
+
         \App\Models\Log::Create($log);
+
         if ($chatId != null) {
+            $log["email"] = \Illuminate\Support\Facades\Auth::user();
+            $log["message"] = $message;
             $telegram = new \Telegram\Bot\Api();
             $response = $telegram->sendMessage([
                 'chat_id' => $chatId,
@@ -40,6 +53,7 @@ class Log
                 // 'parse_mode' => 'html',
             ]);
         }
+
         return $log;
     }
 }
