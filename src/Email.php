351d55f0583451a3824589a959131f0e5778dcd0
Faraz Fallahi <fffaraz@gmail.com>
2020-12-14 21:55:59 -0500

    Update Email.php

--- src/Email.php
+++ src/Email.php
@@ -8,7 +8,10 @@ class Email
     public static function isValid($email): bool
     {
         if (empty($email)) return false;
-        $email = filter_var($email, FILTER_SANITIZE_STRING);
-        return !(filter_var($email, FILTER_VALIDATE_EMAIL) === false);
+        if ($email !== strtolower($email)) return false;
+        if ($email !== filter_var($email, FILTER_SANITIZE_STRING)) return false;
+        if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) return false;
+        // TODO: also check common spelling errors: gmal, gmai, yaho, htmail
+        return true;
     }
 }
