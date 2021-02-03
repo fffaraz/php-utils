5afb59a98231d5520b364115a98d70c4700cc7bd
Faraz Fallahi <fffaraz@gmail.com>
2021-02-03 18:26:03 -0500

    update

--- src/Telegram.php
+++ src/Telegram.php
@@ -12,31 +12,22 @@ class Telegram
             return new \Telegram\Bot\Api();
     }
 
-    public static function message($chat_id, $text)
+    public static function message($chat_id, $text, $parse_mode = '')
     {
         return Telegram::api()->sendMessage([
             'chat_id' => $chat_id,
             'text'    => $text,
-            // 'parse_mode' => 'html',
+            'parse_mode' => $parse_mode, // HTML, Markdown, MarkdownV2
             'disable_web_page_preview' => true,
         ]);
     }
 
-    public static function markdown($chat_id, $text)
-    {
-        return Telegram::api()->sendMessage([
-            'chat_id' => $chat_id,
-            'text'    => $text,
-            'parse_mode' => 'markdown',
-            'disable_web_page_preview' => true,
-        ]);
-    }
-
-    public static function photo($chat_id, $text, $filename)
+    public static function photo($chat_id, $text, $filename, $parse_mode = '')
     {
         return Telegram::api()->sendPhoto([
             'chat_id' => $chat_id,
             'caption' => $text,
+            'parse_mode' => $parse_mode,
             'photo'   => \Telegram\Bot\FileUpload\InputFile::create($filename, 'photo.png'),
         ]);
     }
